30038c6482369d3a9a12617c0822c1a2833e2c16
MadhavChoudhary <madhavtummala@icloud.com>
2019-02-28 11:46:38 +0530

    Added skelitons of all functions

--- fetch.py
+++ fetch.py
@@ -13,7 +13,7 @@ fulfillment_client = df_response_lib.fulfillment_response()
 actions_client = df_response_lib.actions_on_google_response()
 
 # Base route for Database client
-base_url = 'http://2019.almafiesta.com/testing2019/html/ContactFrom_v11/'
+base_url = 'http://2019.almafiesta.com/testing2019/html/ContactFrom_v11/api/'
 
 # To be implemented
 def handle_key_error(error):
@@ -23,8 +23,78 @@ def handle_status_error():
     pass
 
 # Method for returning carousel containing details of all available doctors.
-def handle_available_doctors():
-    endpoint = base_url + 'availableDoctor.php'
+def handle_all_doctors():
+    endpoint = base_url + 'allDoctors.php'
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_all_Beds():
+    endpoint = base_url + 'allBeds.php'
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_patient_appointments(name):
+    endpoint = base_url + 'appointmentByPatient.php?name='+name
+    response = json.loads(requests.get(endpoint,).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_doctors_contact(name):
+    endpoint = base_url + 'contactByDoctor.php?name='+name
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_department_doctors(dept):
+    endpoint = base_url + 'doctorByDepartment.php?dept='+dept
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_fee_doctors(name):
+    endpoint = base_url + 'feeByDoctor.php?name='+name
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_medicines(name):
+    endpoint = base_url + 'medicineByName.php?name='+name
+    response = json.loads(requests.get(endpoint).text)
+    
+    if response['status'] != 1:
+        return handle_status_error()
+
+    return actions_client.basic_card('Hello World', 'This is a card subtitle',
+        'This is a formatted text.')
+
+def handle_schedule_doctors(name):
+    endpoint = base_url + 'scheduleByDoctor.php?name='+name
     response = json.loads(requests.get(endpoint).text)
     
     if response['status'] != 1:
